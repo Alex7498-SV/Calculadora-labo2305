de29d9e27c989c5c0cc4e24e3a3892ef787eadeb
Alex7498-SV <36484792+Alex7498-SV@users.noreply.github.com>
2018-05-24 20:08:24 -0600

    asignacion de accion para cada boton

--- src/GUI/Ventana.java
+++ src/GUI/Ventana.java
@@ -5,6 +5,7 @@
  */
 package GUI;
 
+import AbstractFactory.FactoryProducer;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,51 +19,161 @@ import javax.swing.JTextField;
  * @author LN710Q
  */
 public class Ventana extends JPanel{
+    private int a = 0, b = 0, flagtip = 1, flagint = 0;
+    String aux = "";
     
-    private int WIDTH=420, HEIGHT=200;
+    private int WIDTH=400, HEIGHT=200;
     private int widthTF=120, heightTF=30;
     private int widthBT=50, heightBT=50;
-    JButton btn;
-    JButton btn1;
-    JButton btn2;
-    JButton btn3;
-    JTextField textfield1, textfield2;
+    JButton suma;
+    JButton resta;
+    JButton mult;
+    JButton div;
+    JButton igual;
+    JButton conv;
+    
+    JTextField textfield1, textfield2, result;
     
     public Ventana(){
-        btn = new JButton("+");
-        btn.setBounds(10, 100, widthBT, heightBT);
+        //CREACION DE BOTONES
+        suma = new JButton("+");
+        suma.setBounds(10, 100, widthBT, heightBT);
         
-        btn1 = new JButton("-");
-        btn1.setBounds(100, 100, widthBT, heightBT);
+        resta = new JButton("-");
+        resta.setBounds(70, 100, widthBT, heightBT);
         
-        btn2 = new JButton("x");
-        btn2.setBounds(190, 100, widthBT, heightBT);
+        mult = new JButton("x");
+        mult.setBounds(130, 100, widthBT, heightBT);
         
-        btn3 = new JButton("/");
-        btn3.setBounds(330, 100, widthBT, heightBT);
+        div = new JButton("/");
+        div.setBounds(190, 100, widthBT, heightBT);
         
+        igual = new JButton("=");
+        igual.setBounds(330, 100, widthBT, heightBT);
+        
+        conv = new JButton("conv");
+        conv.setBounds(250, 100, 70, heightBT);
+        
+//      CREACION DE TEXTFIELDS
         textfield1= new JTextField();
         textfield2= new JTextField();
-        textfield1.setBounds(150, 40, widthTF, heightTF);
-        textfield2.setBounds(150, 150, widthTF, heightTF);
-        textfield2.setText("hola");
-        btn.addActionListener(new ActionListener() {
+        result = new JTextField();
+        textfield1.setBounds(15, 40, widthTF, heightTF);
+        textfield2.setBounds(140, 40, widthTF, heightTF);
+        //textfield2.setText("hola");
+        textfield2.setEditable(false);
+        result.setBounds(270, 40, widthTF, heightTF);
+        result.setEditable(false);
+        
+        
+        // ASIGNACION DE ACCION A BOTONES
+        // BOTON SUMA
+        suma.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textfield1.getText();
-                textfield2.setText(textfield1.getText());
+                aux = textfield1.getText();
+                a = Integer.parseInt(aux);
+                aux = "";
+                System.out.println(a);
+                textfield1.setEditable(false);
+                textfield2.setEditable(true);
+                flagint = 1;
+                suma.setEnabled(false);
+                resta.setEnabled(false);
+                mult.setEnabled(false);
+                div.setEnabled(false);
+                conv.setEnabled(false);
+                System.out.println("selector externo");
+                System.out.println(flagtip);
+                System.out.println("selector interno");
+                System.out.println(flagint);
+                }
+        });
+        //BOTON RESTA
+        resta.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                aux = textfield1.getText();
+                a = Integer.parseInt(aux);
+                aux = "";
+                System.out.println(a);
+                textfield1.setEditable(false);
+                textfield2.setEditable(true);
+                flagint = 2;
+                suma.setEnabled(false);
+                resta.setEnabled(false);
+                mult.setEnabled(false);
+                div.setEnabled(false);
+                conv.setEnabled(false);
+                System.out.println("selector externo");
+                System.out.println(flagtip);
+                System.out.println("selector interno");
+                System.out.println(flagint);
+            }
+        } );
+        // BOTON MULT
+        mult.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                aux = textfield1.getText();
+                a = Integer.parseInt(aux);
+                aux = "";
+                System.out.println(a);
+                textfield1.setEditable(false);
+                textfield2.setEditable(true);
+                flagint = 3;
+                suma.setEnabled(false);
+                resta.setEnabled(false);
+                mult.setEnabled(false);
+                div.setEnabled(false);
+                conv.setEnabled(false);
+                System.out.println("selector externo");
+                System.out.println(flagtip);
+                System.out.println("selector interno");
+                System.out.println(flagint);
             }
         });
-         
-        textfield1.setEditable(true);
-        textfield2.setEnabled(false);
+        //BOTON DIV
+        div.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                aux = textfield1.getText();
+                a = Integer.parseInt(aux);
+                aux = "";
+                System.out.println(a);
+                textfield1.setEditable(false);
+                textfield2.setEditable(true);
+                flagint = 4;
+                suma.setEnabled(false);
+                resta.setEnabled(false);
+                mult.setEnabled(false);
+                div.setEnabled(false);
+                conv.setEnabled(false);
+                System.out.println("selector externo");
+                System.out.println(flagtip);
+                System.out.println("selector interno");
+                System.out.println(flagint);
+            }
+        });
         
-        add(btn);
-        add(btn1);
-        add(btn2);
-        add(btn3);
+        igual.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //FactoryProducer.getFactory(flagtip).getop(flagint);
+            }
+        });
+//        textfield1.setEditable(true);
+
+        
+        add(suma);
+        add(resta);
+        add(mult);
+        add(div);
+        add(igual);
+        add(conv);
         add(textfield1);
         add(textfield2);
+        add(result);
         
         setLayout(null);
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
